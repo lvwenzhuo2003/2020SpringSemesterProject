41d3826071e8f2077fa06943770dec0e1dd712af
lvwenzhuo2003 <lvwenzhuo2003@126.com>
2020-06-22 20:19:59 +0800

    First submit, not finished

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,8 @@
+import javax.swing.*;
+
 public class Gui {
+    private JFrame jFrame = new JFrame("New Session");
+    private JMenuBar jMenuBar = new JMenuBar();
     public static void main(String[] args){
         new Gui();
     }

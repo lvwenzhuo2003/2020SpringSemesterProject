fce8007565565afa9f20fcf06a19ad15d3666f60
lvwenzhuo2003 <lvwenzhuo2003@126.com>
2020-06-29 10:49:24 +0800

    Debug: noshowing

--- src/Gui.java
+++ src/Gui.java
@@ -7,18 +7,17 @@ import java.util.ArrayList;
 
 public class Gui implements Serializable, ActionListener {
     private final JFrame     jFrame            = new JFrame("New Session");
-    private final JPanel     comboPanel        = new JPanel();
     private final JPanel     northPanel        = new JPanel();
     private final JPanel     southPanel        = new JPanel();
     private final JPanel     eastPanel         = new JPanel();
     private final JPanel     westPanel         = new JPanel();
     private final JPanel     centerPanel       = new JPanel();
     private final JLabel     title             = new JLabel("Welcome to use management system!");
-    //private JMenuBar          jMenuBar      = new JMenuBar();
+    private JMenuBar          jMenuBar      = new JMenuBar();
     private final JMenu      jMenu             = new JMenu();
-    private final JTextField amount            = new JTextField();
-    private final JTextField productName       = new JTextField();
-    private final JTextField customerName      = new JTextField();
+    private final JTextArea amount            = new JTextArea();
+    private final JTextArea productName       = new JTextArea();
+    private final JTextArea customerName      = new JTextArea();
     private final JButton    confirm           = new JButton();
     private final JButton    records           = new JButton();
     private final JLabel     productNameLabel  = new JLabel("Product Name: ");
@@ -31,43 +30,56 @@ public class Gui implements Serializable, ActionListener {
     }
 
     public Gui() {
-        //init
-        jFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-        comboPanel.setLayout(new BorderLayout());
-        comboPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
-
-
-        //set window in the middle of the screen
+        //init window position
         int windowWidth = jFrame.getWidth();
         int windowHeight = jFrame.getHeight();
         Toolkit kit = Toolkit.getDefaultToolkit();
         Dimension screenSize = kit.getScreenSize();
         int screenWidth = screenSize.width;
         int screenHeight = screenSize.height;
+
+        //init
+        jFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        jFrame.setSize(400,200);
+        jFrame.setLayout(new BorderLayout());
+        //jFrame.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 0));
         jFrame.setLocation(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);
 
         //set title(north)
-        northPanel.add(title);
+        northPanel.setLayout(new BorderLayout());
+        northPanel.add(title,BorderLayout.CENTER);
 
         //set textfields(west)
-        westPanel.add(productName);
-        westPanel.add(amount);
-        westPanel.add(customerName);
+        westPanel.setLayout(new BorderLayout());
+        productName.setLineWrap(false);
+        amount.setLineWrap(false);
+        ;
+        westPanel.add(productName,BorderLayout.NORTH);
+        westPanel.add(amount,BorderLayout.CENTER);
+        westPanel.add(customerName,BorderLayout.SOUTH);
 
         //set textfield titles(east)
-        eastPanel.add(productNameLabel);
-        eastPanel.add(amountLabel);
-        eastPanel.add(customerNameLabel);
+        eastPanel.add(productNameLabel,BorderLayout.NORTH);
+        eastPanel.add(amountLabel,BorderLayout.CENTER);
+        eastPanel.add(customerNameLabel,BorderLayout.SOUTH);
 
         //set buttons(south)
         confirm.addActionListener(this);
         records.addActionListener(this);
+        southPanel.setLayout(new BorderLayout());
+        southPanel.add(confirm,BorderLayout.WEST);
+        southPanel.add(records,BorderLayout.EAST);
 
         //pack up and show
+        jFrame.add(northPanel,BorderLayout.NORTH);
+        jFrame.add(westPanel,BorderLayout.WEST);
+        jFrame.add(eastPanel,BorderLayout.EAST);
+        jFrame.add(southPanel,BorderLayout.SOUTH);
+        jFrame.setLayout(new BorderLayout());
+        //jFrame.setLayout(new BorderLayout());
+        //jFrame.add(jFrame,BorderLayout.CENTER);
         jFrame.setVisible(true);
-        jFrame.add(northPanel);
-        jFrame.add(comboPanel);
-        jFrame.pack();
+        //jFrame.pack();
     }
 
     public ArrayList<Order> read() {
@@ -76,6 +88,8 @@ public class Gui implements Serializable, ActionListener {
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("./temp/record.ser"));
             recording = (ArrayList<Order>) in.readObject();
             in.close();
+        } catch (EOFException ex){
+            recording = new ArrayList<Order>();
         } catch (FileNotFoundException ex) {
             JOptionPane.showMessageDialog(null, "File not found");
         } catch (IOException ex) {
@@ -87,14 +101,16 @@ public class Gui implements Serializable, ActionListener {
         return recording;
     }
 
-    public void write(ArrayList<String> record) {
+    public void write(ArrayList<Order> record) {
         try {
             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("./temp/record.ser"));
             out.writeObject(record);
             out.close();
             JOptionPane.showMessageDialog(null, "Serialized data write successfully!");
         } catch (IOException exp) {
-            exp.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Internal error. ");
+        } catch (NumberFormatException exp){
+            JOptionPane.showMessageDialog(null, "Incorrect input type! ");
         }
     }
 
@@ -107,8 +123,10 @@ public class Gui implements Serializable, ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == confirm) {
             record.add(new Order(productName.getText(), Integer.parseInt(amount.getText()), customerName.getText()));
+            write(record);
+            JOptionPane.showMessageDialog(null, "Successfully saved file. ");
         } else if (e.getSource() == records) {
-
+            jFrame.setVisible(false);
         }
     }
 }

0015801ed5186b244fc77a3d2576446b670c6358
lvwenzhuo2003 <lvwenzhuo2003@126.com>
2020-09-03 14:24:01 +0800

    Final

--- src/Gui.java
+++ src/Gui.java
@@ -21,21 +21,21 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Gui implements Serializable, ActionListener {
-    private final JFrame jFrame = new JFrame("New Session");
-    private final JPanel comboPanel = new JPanel();
-    private final JPanel northPanel = new JPanel();
-    private final JPanel southPanel = new JPanel();
-    private final JPanel eastPanel = new JPanel();
-    private final JPanel westPanel = new JPanel();
-    private final JLabel title = new JLabel("Welcome to use management system!");
-    private final JTextArea amount = new JTextArea();
-    private final JTextArea productName = new JTextArea();
-    private final JTextArea customerName = new JTextArea();
-    private final JButton confirm = new JButton();
-    private final JButton records = new JButton();
-    private final JLabel productNameLabel = new JLabel("Product Name: ");
-    private final JLabel amountLabel = new JLabel("Amount: ");
-    private final JLabel customerNameLabel = new JLabel("Customer Name: ");
+    private final JFrame    jFrame            = new JFrame("New Session");
+    private final JPanel    comboPanel        = new JPanel();
+    private final JPanel    northPanel        = new JPanel();
+    private final JPanel    southPanel        = new JPanel();
+    private final JPanel    eastPanel         = new JPanel();
+    private final JPanel    westPanel         = new JPanel();
+    private final JLabel    title             = new JLabel("Welcome to use management system!");
+    private final JTextArea amount            = new JTextArea();
+    private final JTextArea productName       = new JTextArea();
+    private final JTextArea customerName      = new JTextArea();
+    private final JButton   confirm           = new JButton();
+    private final JButton   records           = new JButton();
+    private final JLabel    productNameLabel  = new JLabel("Product Name: ");
+    private final JLabel    amountLabel       = new JLabel("Amount: ");
+    private final JLabel    customerNameLabel = new JLabel("Customer Name: ");
     ArrayList<Order> record = read();
 
     public static void main(String[] args) {
@@ -44,8 +44,8 @@ public class Gui implements Serializable, ActionListener {
 
     public Gui() {
         jFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-        jFrame.setSize(400,200);
-        comboPanel.setLayout(new BorderLayout(10,10));
+        jFrame.setSize(400, 200);
+        comboPanel.setLayout(new BorderLayout(10, 10));
         int windowWidth = jFrame.getWidth();
         int windowHeight = jFrame.getHeight();
         Toolkit kit = Toolkit.getDefaultToolkit();
@@ -55,36 +55,36 @@ public class Gui implements Serializable, ActionListener {
         jFrame.setLocation(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);
 
         //set title(north)
-        northPanel.setLayout(new BorderLayout(10,10));
-        northPanel.add(title,BorderLayout.CENTER);
+        northPanel.setLayout(new BorderLayout(10, 10));
+        northPanel.add(title, BorderLayout.CENTER);
 
         //set textfields(west)
-        westPanel.setLayout(new BorderLayout(10,10));
+        westPanel.setLayout(new BorderLayout(10, 10));
         productName.setLineWrap(false);
         amount.setLineWrap(false);
 
-        westPanel.add(productName,BorderLayout.NORTH);
-        westPanel.add(amount,BorderLayout.CENTER);
-        westPanel.add(customerName,BorderLayout.SOUTH);
+        westPanel.add(productName, BorderLayout.NORTH);
+        westPanel.add(amount, BorderLayout.CENTER);
+        westPanel.add(customerName, BorderLayout.SOUTH);
 
         //set textfield titles(east)
-        eastPanel.setLayout(new BorderLayout(10,10));
-        eastPanel.add(productNameLabel,BorderLayout.NORTH);
-        eastPanel.add(amountLabel,BorderLayout.CENTER);
-        eastPanel.add(customerNameLabel,BorderLayout.SOUTH);
+        eastPanel.setLayout(new BorderLayout(10, 10));
+        eastPanel.add(productNameLabel, BorderLayout.NORTH);
+        eastPanel.add(amountLabel, BorderLayout.CENTER);
+        eastPanel.add(customerNameLabel, BorderLayout.SOUTH);
 
         //set buttons(south)
         confirm.addActionListener(this);
         records.addActionListener(this);
-        southPanel.setLayout(new BorderLayout(10,10));
-        southPanel.add(confirm,BorderLayout.WEST);
-        southPanel.add(records,BorderLayout.EAST);
+        southPanel.setLayout(new BorderLayout(10, 10));
+        southPanel.add(confirm, BorderLayout.WEST);
+        southPanel.add(records, BorderLayout.EAST);
 
         //pack up and show
-        comboPanel.add(northPanel,BorderLayout.NORTH);
-        comboPanel.add(westPanel,BorderLayout.WEST);
-        comboPanel.add(eastPanel,BorderLayout.EAST);
-        comboPanel.add(southPanel,BorderLayout.SOUTH);
+        comboPanel.add(northPanel, BorderLayout.NORTH);
+        comboPanel.add(westPanel, BorderLayout.WEST);
+        comboPanel.add(eastPanel, BorderLayout.EAST);
+        comboPanel.add(southPanel, BorderLayout.SOUTH);
         jFrame.add(comboPanel);
         jFrame.setVisible(true);
         jFrame.pack();
@@ -96,7 +96,7 @@ public class Gui implements Serializable, ActionListener {
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("./temp/record.ser"));
             recording = (ArrayList<Order>) in.readObject();
             in.close();
-        } catch (EOFException ex){
+        } catch (EOFException ex) {
             recording = new ArrayList<Order>();
         } catch (FileNotFoundException ex) {
             JOptionPane.showMessageDialog(null, "File not found");
@@ -117,7 +117,7 @@ public class Gui implements Serializable, ActionListener {
             JOptionPane.showMessageDialog(null, "Serialized data write successfully!");
         } catch (IOException exp) {
             JOptionPane.showMessageDialog(null, "Internal error. ");
-        } catch (NumberFormatException exp){
+        } catch (NumberFormatException exp) {
             JOptionPane.showMessageDialog(null, "Incorrect input type! ");
         }
     }
